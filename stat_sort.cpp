98d399ca9957e48336d58765359409bf06a66fd1
menabebawy1 <mena.m.bebawy@gmail.com>
2020-12-30 16:56:16 -0500

    added mean and standard deviation functions

---
+++ stat_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double mean(double arr[], double size)
+{
+    double sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
+double stddev(double arr[], int size)
+{
+    double total = 0;
+    double average = mean(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        total += pow((average - arr[i]), 2);
+    }
+    return sqrt(total / size);
+}
+
+int main()
+{
+    double nums[20] = {9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4};
+    cout << stddev(nums, 20) << endl;
+}
