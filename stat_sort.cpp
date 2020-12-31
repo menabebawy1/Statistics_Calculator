d137f481c6365a3c7d6fe6aca80fbbfd55a99988
menabebawy1 <mena.m.bebawy@gmail.com>
2020-12-30 19:27:23 -0500

    added min, max, and sort functions

--- stat_sort.cpp
+++ stat_sort.cpp
@@ -50,13 +50,86 @@ int combination(int n, int k)
     return factorial(n) / (factorial(k) * (factorial(n - k)));
 }
 
-double binomialRandomVariable(double n, double x, double p){
-    return combination(n,x)*pow(p,x)*pow((1-p),(n-x));
+double* insertionSort(double arr[], int size)
+{
+    double *p = new double[size];
+
+    for (int i = 0 ; i < size; i++){
+        p[i] = arr[i];
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+
+        if (p[i + 1] < p[i])
+        {
+            swap(p[i], p[i + 1]);
+        }
+
+        for (int x = i; x > 0; x--)
+        {
+            if (p[x] < p[x - 1])
+            {
+                swap(p[x], p[x - 1]);
+            }
+        }
+    }
+    
+    return p;
+
+}
+
+double max(double arr[], int size)
+{
+    double max = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+double min(double arr[], int size)
+{
+    double min = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+double median(double arr[], int size){
+    double *p = insertionSort(arr, size);
+    for(int i = 0; i < size; i++){
+        cout << p[i] << " ";
+    }
+    cout << endl;
+    delete p;
+    p = nullptr;
+}
+
+void BoxWhisker(double arr[], int size)
+{
+}
+
+double binomialRandomVariable(double n, double x, double p)
+{
+    return combination(n, x) * pow(p, x) * pow((1 - p), (n - x));
 }
 
 int main()
 {
     double nums[20] = {9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4};
-    cout << combination(7, 3) << endl;
-    cout << binomialRandomVariable(12,8,.6) << endl;
+    //cout << combination(7, 3) << endl;
+   // cout << min(nums, 20) << endl;
+    //cout << binomialRandomVariable(12, 8, .6) << endl;
+   cout << median(nums, 20) << endl;
+    
 }
